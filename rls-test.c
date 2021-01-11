f71b63acfaf5027f07314ca1ef7a6900244e6d7f
carlogattuso <carlogc46@gmail.com>
2021-01-11 13:42:29 +0100

    grid allocation done

--- rls-test.c
+++ rls-test.c
@@ -4,6 +4,15 @@
 #include <stdlib.h>
 #include <math.h>
 
+#define QAM4_LEVEL      0.7071    ///< QAM4 amplitude (RMS=1)
+#define BUFFER_SZ	2048
+#define MOD_NO_DMRS_LENGTH 432
+#define MOD_DMRS_LENGTH 504
+
+#define CARRIERS 156
+#define SYMBOLS 14
+#define NUM_SUBFRAMES 1
+
 int mod_BPSK (int numbits, _Complex float *symbols);
 int genRSsignalargerThan3RB(int u, int v, int m, int M_RS_SC, _Complex float *DMRSseq, int TxRxMode);
 int largestprime_lower_than(int number);
@@ -11,21 +20,17 @@ int check_if_prime(int number);
 void printZFCOEFF(_Complex float *correct, int nofcoeff);
 void printCOEFF(int *correct, int nofcoeff);
 int allocTest (int numbits, int *test);
-void gridAllocation(_Complex float *grid, _Complex float *symbols, int rows, int columns, int symbLength);
-
-#define QAM4_LEVEL      0.7071    ///< QAM4 amplitude (RMS=1)
-#define BUFFER_SZ	2048
-#define MOD_NO_DMRS_LENGTH 432
-#define MOD_DMRS_LENGTH 504
-#define CARRIERS 12
-#define SYMBOLS 14
-#define NUM_SUBFRAMES 1
+void gridAllocationTest(int matrix[CARRIERS][SYMBOLS*NUM_SUBFRAMES], int *vector);
+void gridAllocation(_Complex float grid[CARRIERS][SYMBOLS*NUM_SUBFRAMES], _Complex float *symbols);
+void printMatrixTest(int matrix[CARRIERS][SYMBOLS*NUM_SUBFRAMES]);
+void printMatrix(_Complex float matrix[CARRIERS][SYMBOLS*NUM_SUBFRAMES]);
 
 //Modulation symbols and grid
 _Complex float symbols[BUFFER_SZ];
 _Complex float grid[CARRIERS][SYMBOLS*NUM_SUBFRAMES];
 
 int test[BUFFER_SZ];
+int testMatrix[CARRIERS][SYMBOLS*NUM_SUBFRAMES];
 
 //DMRS Params
 int M_RS_SC = 156;
@@ -34,16 +39,28 @@ _Complex float DMRS_SEQ0[BUFFER_SZ];
 _Complex float DMRS_SEQ1[BUFFER_SZ];
 
 int main() {
-    //We modulate one subframe of samples
-    //mod_BPSK(MOD_DMRS_LENGTH, symbols);
-	allocTest(MOD_DMRS_LENGTH, test);
-    printCOEFF(test, CARRIERS*SYMBOLS);
+	allocTest(CARRIERS*SYMBOLS*NUM_SUBFRAMES, test);
+	printf("Vector: \n");
+    printCOEFF(test, CARRIERS*SYMBOLS*NUM_SUBFRAMES);
+	
+	gridAllocationTest(testMatrix, test);
+	printf("Matrix: \n");
+	printMatrixTest(testMatrix);
+
+	//We modulate one subframe of samples
+    mod_BPSK(CARRIERS*SYMBOLS*NUM_SUBFRAMES, symbols);
+	printf("Vector: \n");
+	printZFCOEFF(symbols, CARRIERS*SYMBOLS*NUM_SUBFRAMES);
+
+	//We allocate grid received into a matrix to equalize each subcarrier
+	gridAllocation(grid, symbols);
+	printMatrix(grid);
 
     DMRS_length = genRSsignalargerThan3RB(0, 1, 10, M_RS_SC, DMRS_SEQ0, 0);
     printf("DMRS 0 Length: %d\n", DMRS_length);
     DMRS_length = genRSsignalargerThan3RB(1, 1, 10, M_RS_SC, DMRS_SEQ1, 0);
     printf("DMRS 1 Length: %d\n", DMRS_length);
-	
+
     return 0;
 }
 
@@ -57,6 +74,25 @@ int allocTest (int numbits, int *test){
 	}
 	return(i);
 }
+
+void printMatrixTest(int matrix[CARRIERS][SYMBOLS*NUM_SUBFRAMES]){
+	int i, j; 
+    for (i = 0; i < CARRIERS; i++){
+		for (j = 0; j < (SYMBOLS*NUM_SUBFRAMES); j++) 
+			printf("%d	", matrix[i][j]); 
+		printf("\n");
+	}
+} 
+
+void printMatrix(_Complex float matrix[CARRIERS][SYMBOLS*NUM_SUBFRAMES]){
+	int i, j; 
+    for (i = 0; i < CARRIERS; i++){
+		for (j = 0; j < (SYMBOLS*NUM_SUBFRAMES); j++) 
+			if(__imag__ matrix[i][j] >= 0.0)printf("%f+%f*I,	", __real__ matrix[i][j],  __imag__ matrix[i][j]);
+			else printf("%f%f*I,	", __real__ matrix[i][j],  __imag__ matrix[i][j]);
+		printf("\n");
+	}
+} 
 
 int mod_BPSK (int numbits, _Complex float *symbols){
 	int i, bit;
@@ -137,11 +173,20 @@ void printCOEFF(int *correct, int nofcoeff){
 	}
 }
 
-void gridAllocation(_Complex float *grid, _Complex float *symbols, int rows, int columns, int symbLength){
+void gridAllocationTest(int matrix[CARRIERS][SYMBOLS*NUM_SUBFRAMES], int *vector){
 	int i,j;
 	for(i=0; i<(SYMBOLS*NUM_SUBFRAMES); i++){
 		for(j=0; j<CARRIERS; j++){
-			//grid[j][i] = symbols[CARRIERS*i+j];
+			matrix[j][i] = vector[CARRIERS*i+j];
+		}
+	}
+}
+
+void gridAllocation(_Complex float grid[CARRIERS][SYMBOLS*NUM_SUBFRAMES], _Complex float *symbols){
+	int i,j;
+	for(i=0; i<(SYMBOLS*NUM_SUBFRAMES); i++){
+		for(j=0; j<CARRIERS; j++){
+			grid[j][i] = symbols[CARRIERS*i+j];
 		}
 	}
 }
